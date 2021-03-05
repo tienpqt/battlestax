d99877731ed38f38523319ffae818ad70e9b750f
Duyen Nguyen <duyennguyen@Mac-mini-de-Duyen.local>
2021-03-05 15:55:34 +0100

    Merging helloWorld PTE into master

--- functions/helloWorld.js
+++ functions/helloWorld.js
@@ -2,6 +2,6 @@ exports.handler = async (event, context) => {
   // let's return a JSON response that looks like: { hello: "world" }
   return {
     statusCode: 200,
-    body: JSON.stringify({ hello: "world" }),
+    body: JSON.stringify({ hello: "world PTE" }),
   };
 };
